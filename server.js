5304257e78158e0768c5c24ce821e500c179927a
NhonPham <nhonpham0211@gmail.com>
2021-02-20 08:21:50 +0700

    Updated server.js and .env

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// Routes
+app.use("/user", require("./routes/userRouter"));
+
+//Connect to mogodb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+  }
+);
 
 app.get("/", (req, res) => {
   res.json({
