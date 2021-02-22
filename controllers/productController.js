98bc85899c2381c16ec1440f87be8b55016b3bc7
ntnhan997 <ntnhan997@gmail.com>
2021-02-22 23:48:03 +0700

    update productController.js (Update Product)

--- controllers/productController.js
+++ controllers/productController.js
@@ -43,6 +43,28 @@ const productController = {
           return res.status(500).json({ msg: err.message });
         }
       },
+      updateProduct: async (req, res) => {
+        try {
+          const { title, price, description, content, images, category } = req.body;
+          if (!images) return res.status(400).json({ msg: "No image upload." });
+    
+          await Products.findByIdAndUpdate(
+            { _id: req.params.id },
+            {
+              title: title.toLowerCase(),
+              price,
+              description,
+              content,
+              images,
+              category,
+            }
+          );
+    
+          res.json({ msg: "Updated a Product." });
+        } catch (err) {
+          return res.status(500).json({ msg: err.message });
+        }
+      },
 }
 
 
