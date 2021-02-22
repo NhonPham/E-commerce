703c3ffb431f8748a17eff05ce8e5be0a491277b
NhonPham <nhonpham0211@gmail.com>
2021-02-22 16:05:46 +0700

    updated userRouter.js (refreshToken)

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 
 router.post("/register", userController.register);
+
 router.post("/login", userController.login);
+
+router.get("/refresh_token", userController.refreshToken);
 
 module.exports = router;
