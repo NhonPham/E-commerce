860a5f091b237830e7a16c8c7873c914029fec48
NhonPham <nhonpham0211@gmail.com>
2021-02-23 12:36:51 +0700

    updated routes/userRouter.js (logout)

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,8 @@ router.post("/register", userController.register);
 
 router.post("/login", userController.login);
 
+router.get("/logout", userController.logout);
+
 router.get("/refresh_token", userController.refreshToken);
 
 module.exports = router;
