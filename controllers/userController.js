bf1e0e69acc95b5d19639f4e28e467d4336ffb25
NhonPham <nhonpham0211@gmail.com>
2021-02-23 12:34:36 +0700

    updated userController.js (logout)

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,6 +67,14 @@ const userController = {
       return res.status(500).json({ msg: err.message });
     }
   },
+  logout: async (req, res) => {
+    try {
+      res.clearCookie("refreshtoken", { path: "/user/refresh_token" });
+      return res.json({ msg: "Logged out" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
   refreshToken: (req, res) => {
     try {
       const rf_token = req.cookies.refreshtoken;
