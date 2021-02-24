375522c9bf9d0d457d1d9065d5b6fdba5efd7a52
NhonPham <nhonpham0211@gmail.com>
2021-02-24 16:28:57 +0700

    updated userController.js(getUser)

--- controllers/userController.js
+++ controllers/userController.js
@@ -94,6 +94,16 @@ const userController = {
       return res.status(500).json({ msg: err.message });
     }
   },
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.user.id).select("-password");
+      if (!user) return res.status(400).jsom({ msg: "User does not exist." });
+
+      res.json(user);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 const createAccessToken = (user) => {
