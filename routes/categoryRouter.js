c43aa9c1a0a9b25447510cbf3ea873096c287452
DESKTOP-9BB6PK7\Khang <nguyetpham180397@gmail.com>
2021-03-19 23:36:40 +0700

    updated route category (createCategory)

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const categoryController = require("../controllers/categoryController");
-
+const auth = require("../middleWare/auth");
+const authAdmin = require("../middleWare/authAdmin");
 
 router
   .route("/category")
-  .get(categoryController.getCategories);
+  .get(categoryController.getCategories)
+  .post(auth, authAdmin, categoryController.createCategory);
 
 module.exports = router;
