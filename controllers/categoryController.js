22860cdf49ddbe686604842254ceb72d817f4221
DESKTOP-9BB6PK7\Khang <nguyetpham180397@gmail.com>
2021-03-19 23:45:11 +0700

    updated categoryController (createCategory)

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -9,7 +9,25 @@ const categoryController = {
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
-  }
+  },
+
+  createCategory: async (req, res) => {
+    try {
+      //if user have role = 1 => admin
+      //only admin can create, delete, update category
+      const { name } = req.body;
+      const category = await Category.findOne({ name });
+      if (category)
+        return res.status(400).json({ msg: "This category already exist." });
+
+      const newCategory = new Category({ name });
+      await newCategory.save();
+
+      res.json({ msg: "Created a category" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 module.exports = categoryController;
