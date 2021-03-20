958ad75363b04f09d0307b653ca9ef837ac38573
NhonPham <nhonpham0211@gmail.com>
2021-03-20 19:11:54 +0700

    updated userController(history)

--- controllers/userController.js
+++ controllers/userController.js
@@ -121,6 +121,15 @@ const userController = {
       res.status(500).json({ msg: err.message });
     }
   },
+  history: async (req, res) => {
+    try {
+      const history = await Payments.find({ user_id: req.user.id });
+
+      res.json(history);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 const createAccessToken = (user) => {
