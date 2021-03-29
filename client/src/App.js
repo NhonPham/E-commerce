5d39a0585f2a5d8e7178c82770d7d38b94f6c9cf
NhonPham <nhonpham0211@gmail.com>
2021-03-29 12:00:07 +0700

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./GlobalState";
+import Header from "./components/headers/Header";
+import MainPages from "./components/mainpages/Pages";
+import Footer from "./components/footers/Footer";
+import ScollToTop from "./ScrollToTop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">hello world</header>
-    </div>
+    <DataProvider>
+      <Router>
+        <ScollToTop />
+        <div className="App">
+          <Header />
+          <MainPages />
+          <Footer />
+        </div>
+      </Router>
+    </DataProvider>
   );
 }
 
